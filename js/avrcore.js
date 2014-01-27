2c0fad35a1178e1ace56af8a7f5d2a662ca6226d
Blake W. Ford <blakewford@gmail.com>
2014-01-27 00:31:54 -0600

    Fix bug with com instruction

--- js/avrcore.js
+++ js/avrcore.js
@@ -366,7 +366,7 @@ function fetch(b, a) {
         break;
     case 148:
     case 149:
-        if (0 === (a & 255)) r[c] = 255 - r[c];
+        if (0 === (a & 15)) r[c] = 255 - r[c];
         else if (2 === (a & 255)) r[c] = r[c] << 4 | r[c] >> 4;
         else if (3 === (a & 255)) r[c] += 1;
         else if (5 === (a & 255)) d = r[c], g = d & 1, d = d >> 1 | d & 128, setPostEvaluationFlags(d), r[c] = d, C = g, V = N ^ C;
