d1920d0e29eebd80710a5b1a3d08e581fb754b9f
Blake W. Ford <blakewford@gmail.com>
2014-04-10 01:32:57 -0500

    Clean up UI. Start getting some screen code in order

--- android/src/org/starlo/boardmicro/MainActivity.java
+++ android/src/org/starlo/boardmicro/MainActivity.java
@@ -10,16 +10,44 @@ import android.content.Intent;
 import android.webkit.WebViewClient;
 import com.dropbox.chooser.android.DbxChooser;
 
-public class MainActivity extends Activity {
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.util.Log;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
 	static final int DBX_CHOOSER_REQUEST = 0;
 	private WebView mBackgroundWebView;
 	private boolean mDropboxCalled = false;
 
+	private static final int SCREEN_WIDTH = 160;
+	private static final int SCREEN_HEIGHT = 128;
+	private SurfaceHolder mHolder;
+	private Bitmap mBitmap;
+	private Bitmap mScaledBitmap;
+	private int mScreenWidth;
+	private int mScreenHeight;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.display_layout);
+		SurfaceView surfaceView = (SurfaceView)findViewById(R.id.display);
+		Resources r = surfaceView.getResources();
+		mHolder = surfaceView.getHolder();
+		mHolder.addCallback(this);
+		mBitmap = Bitmap.createBitmap(SCREEN_WIDTH, SCREEN_HEIGHT, Config.ARGB_8888);
+		mScreenWidth =
+			Float.valueOf(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, r.getConfiguration().screenWidthDp, r.getDisplayMetrics())).intValue();		setContentView(R.layout.display_layout);
+		mScreenHeight =
+			Float.valueOf(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, r.getConfiguration().screenHeightDp, r.getDisplayMetrics())).intValue();
 		mBackgroundWebView = new WebView(this);
 		mBackgroundWebView.getSettings().setJavaScriptEnabled(true);
 		mBackgroundWebView.loadUrl("file:///android_asset/example.html");
@@ -50,6 +78,33 @@ public class MainActivity extends Activity {
 			}
 		} else {
 			super.onActivityResult(requestCode, resultCode, data);
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		wipeScreen();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {}
+
+	private void wipeScreen(){
+		if(mScaledBitmap != null)
+			mScaledBitmap.recycle();
+		Canvas canvas = mHolder.lockCanvas();
+		if(canvas != null){
+			int[] colors = new int[SCREEN_WIDTH*SCREEN_HEIGHT];
+			for(int i = 0; i < colors.length; i++)
+				colors[i] = Color.BLUE;
+			canvas.drawColor(Color.BLACK);
+			mBitmap.setPixels(colors, 0, SCREEN_WIDTH, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+			mScaledBitmap = Bitmap.createScaledBitmap(mBitmap, mScreenWidth, mScreenWidth, false);
+			canvas.drawBitmap(mScaledBitmap, 0, 0, null);
+			mHolder.unlockCanvasAndPost(canvas);
 		}
 	}
 }
