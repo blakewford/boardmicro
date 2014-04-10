4ac55772d8256105e82690b4ed51ccb9a1dd13e9
Blake W. Ford <blakewford@gmail.com>
2014-04-09 20:56:38 -0500

    Switch to simple Dropbox Chooser

--- android/src/org/starlo/boardmicro/MainActivity.java
+++ android/src/org/starlo/boardmicro/MainActivity.java
@@ -5,11 +5,16 @@ import org.starlo.boardmicro.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.webkit.WebView;
+import android.widget.Toast;
+import android.content.Intent;
 import android.webkit.WebViewClient;
+import com.dropbox.chooser.android.DbxChooser;
 
 public class MainActivity extends Activity {
 
+	static final int DBX_CHOOSER_REQUEST = 0;
 	private WebView mBackgroundWebView;
+	private boolean mDropboxCalled = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,26 @@ public class MainActivity extends Activity {
                 mBackgroundWebView.getSettings().setUserAgentString(mBackgroundWebView.getSettings().getUserAgentString()+" NativeApp");
 		mBackgroundWebView.setWebViewClient(new WebViewClient() {
 			public void onPageFinished(WebView view, String loc) {
-				view.loadUrl("javascript:loadDefault()");
-				view.loadUrl("javascript:engineInit()");
-				view.loadUrl("javascript:exec()");
+				if(!mDropboxCalled){
+					mDropboxCalled = true;
+					new DbxChooser("").forResultType(DbxChooser.ResultType.FILE_CONTENT).launch(MainActivity.this, DBX_CHOOSER_REQUEST);
+				}
 			}
 		});
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == DBX_CHOOSER_REQUEST) {
+			if (resultCode == Activity.RESULT_OK) {
+				mBackgroundWebView.loadUrl("javascript:loadDefault()");
+				mBackgroundWebView.loadUrl("javascript:engineInit()");
+				mBackgroundWebView.loadUrl("javascript:exec()");
+				DbxChooser.Result result = new DbxChooser.Result(data);
+				Toast.makeText(this, result.getLink().toString(), Toast.LENGTH_SHORT).show();
+			}
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
 	}
 }
