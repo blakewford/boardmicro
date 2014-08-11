3255290d1105a7695f9bb16843e9a005c54b87ab
blakewford <blakewford@gmail.com>
2014-08-11 00:32:04 -0500

    Start working on instructions

--- js/armcore.js
+++ js/armcore.js
@@ -44,13 +44,58 @@ function loadMemory(c, b) {
   }
   console.log(PC);
 }
-function fetch(c) {
+
+//0 0 0 Op Offset5 Rs Rd Move shifted register
+//0 0 0 1 1 I Op Rn/offset3 Rs Rd Add/subtract
+//0 0 1 Op Rd Offset8 Move/compare/add/subtract immediate
+//0 1 0 0 0 0 Op Rs Rd ALU operations
+//0 1 0 0 0 1 Op H1 H2 Rs/Hs Rd/Hd Hi register operations/branch exchange
+//0 1 0 0 1 Rd Word8 PC-relative load
+//0 1 0 1 L B 0 Ro Rb Rd Load/store with register offset
+//0 1 0 1 H S 1 Ro Rb Rd Load/store sign-extended byte/halfword
+//0 1 1 B L Offset5 Rb Rd Load/store with immediate offset
+//1 0 0 0 L Offset5 Rb Rd Load/store halfword
+//1 0 0 1 L Rd Word8 SP-relative load/store
+//1 0 1 0 SP Rd Word8 Load address
+//1 1 0 0 L Rb Rlist Multiple load/store
+//1 1 0 1 Cond Soffset8 Conditional branch
+//1 1 0 1 1 1 1 1 Value8 Software Interrupt
+//1 1 1 0 0 Offset11 Unconditional branch
+//1 1 1 1 H Offset Long branch with link
+
+function offsetStack(byte){
+  //S SWord7 Add offset to stack pointer
+}
+function pushpop(){
+  //Rlist
+  switch(options){
+    case 4:
+      break;
+    case 5:
+      break;
+    case 12:
+      break;
+    case 13:
+      break;
+  }
+}
+
+function fetch(c, data) {
   if(c < 128){
     switch(c) {
     }
   }
   else if(c >= 128 && c < 256){
     switch(c){
+      case 176:
+        offsetStack(data);
+        break;
+      case 180:
+      case 181:
+      case 188:
+      case 189:
+        pushpop(op & 0xF, data);
+        break;
     }
   }
   else{
