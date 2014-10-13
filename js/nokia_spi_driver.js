5f1f74885426a714efcef6717e8bb222ef809676
blakewford <blakewford@gmail.com>
2014-10-12 22:38:58 -0500

    First pass PCD8544 driver

--- js/nokia_spi_driver.js
+++ js/nokia_spi_driver.js
@@ -1,3 +1,22 @@
+var x=0, y=0;
+var IsImportant = false;
 function peripheralSPIWrite(a) {
-  alert(a);
+  var LCDHEIGHT_NOROT 48
+  var LCDWIDTH_NOROT 84
+  var PCD8544_SETYADDR = 0x40;
+  var PCD8544_SETXADDR = 0x80;
+  if(IsImportant && a != PCD8544_SETXADDR && (a < PCD8544_SETYADDR || a > PCD8544_SETYADDR+5))
+  {
+      x++;
+      if(x == LCDWIDTH_NOROT)
+        x = 0;
+  }
+  if(a == PCD8544_SETYADDR)
+  {
+      IsImportant=!IsImportant;
+  }
+  if(a >= PCD8544_SETYADDR && a <= PCD8544_SETYADDR+5)
+  {
+      y = a - PCD8544_SETYADDR;
+  }
 }
