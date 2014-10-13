0c6bda137c77aaf1e8624716635142d0e78b83fe
blakewford <blakewford@gmail.com>
2014-10-13 01:05:08 -0500

    Get something working

--- js/nokia_spi_driver.js
+++ js/nokia_spi_driver.js
@@ -1,12 +1,18 @@
 var x=0, y=0;
 var IsImportant = false;
 function peripheralSPIWrite(a) {
-  var LCDHEIGHT_NOROT 48
-  var LCDWIDTH_NOROT 84
+  var LCDHEIGHT_NOROT = 48;
+  var LCDWIDTH_NOROT = 84;
   var PCD8544_SETYADDR = 0x40;
   var PCD8544_SETXADDR = 0x80;
   if(IsImportant && a != PCD8544_SETXADDR && (a < PCD8544_SETYADDR || a > PCD8544_SETYADDR+5))
   {
+      for(var i=0; i < 8; i++)
+      {
+        var bit = a & (1 << i);
+        var color = bit == 0 ? "#000000":"#FFFFFF"
+        drawPixel(x, y+i, color);
+      }
       x++;
       if(x == LCDWIDTH_NOROT)
         x = 0;
@@ -17,6 +23,6 @@ function peripheralSPIWrite(a) {
   }
   if(a >= PCD8544_SETYADDR && a <= PCD8544_SETYADDR+5)
   {
-      y = a - PCD8544_SETYADDR;
+      y = (a - PCD8544_SETYADDR)*8;
   }
 }
