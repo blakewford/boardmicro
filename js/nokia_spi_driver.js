150683aba0bcfa0c289467d6697507f9a0c499d3
blakewford <blakewford@gmail.com>
2014-10-15 21:53:23 -0500

    Add proper pin signaling

--- js/nokia_spi_driver.js
+++ js/nokia_spi_driver.js
@@ -1,12 +1,11 @@
 var x=0, y=0;
-var IsImportant = false;
 fillScreen("#FFFFFF");
 function peripheralSPIWrite(a) {
   var LCDHEIGHT_NOROT = 48;
   var LCDWIDTH_NOROT = 84;
   var PCD8544_SETYADDR = 0x40;
   var PCD8544_SETXADDR = 0x80;
-  if(IsImportant && a != PCD8544_SETXADDR && (a < PCD8544_SETYADDR || a > PCD8544_SETYADDR+5))
+  if(readMemory(portC) == 5)
   {
       for(var i=0; i < 8; i++)
       {
@@ -15,15 +14,13 @@ function peripheralSPIWrite(a) {
         drawPixel(x, y+i, color);
       }
       x++;
-      if(x == LCDWIDTH_NOROT)
-        x = 0;
   }
-  if(a == PCD8544_SETYADDR)
-  {
-      IsImportant=!IsImportant;
-  }
-  if(a >= PCD8544_SETYADDR && a <= PCD8544_SETYADDR+5)
+  if(a >= PCD8544_SETYADDR && a <= PCD8544_SETYADDR+5 && (readMemory(portC) == 1))
   {
       y = (a - PCD8544_SETYADDR)*8;
+  }
+  if(a == PCD8544_SETXADDR && (readMemory(portC) == 1))
+  {
+      x = 0;
   }
 }
