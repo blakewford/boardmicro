45600088fe94ba7de46db316a229f4121e70985d
blakewford <blakewford@gmail.com>
2014-11-15 17:50:51 -0600

    Write Nokia screen data to the DMA Region

--- js/nokia_spi_driver.js
+++ js/nokia_spi_driver.js
@@ -11,7 +11,9 @@ function peripheralSPIWrite(a) {
         var bit = a & (1 << i);
         var color = bit == 0 ? "#000000":"#FFFFFF"
         drawPixel(x, y+i, color);
+        writeMemory(32758+i, bit);
       }
+      writeMemory(32766, x);
       x++;
   }
   if(a >= PCD8544_SETYADDR && a <= PCD8544_SETYADDR+5 && (readMemory(portC) == 1))
@@ -20,6 +22,7 @@ function peripheralSPIWrite(a) {
   }
   if(a == PCD8544_SETXADDR && (readMemory(portC) == 1))
   {
+      //writeMemory(32767, 0);
       x = 0;
   }
 }
