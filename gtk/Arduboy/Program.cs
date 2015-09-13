e3364819b47560398a4ec4fe1c07f9084399e730
Blake W. Ford <blakewford@gmail.com>
2015-09-13 12:33:15 -0500

    Add interactions to native code

--- gtk/Arduboy/Program.cs
+++ gtk/Arduboy/Program.cs
@@ -1,17 +1,38 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 using Gtk;
 
 namespace Arduboy
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		private static volatile bool GtkRunning = true;
+
+		private static void Simulation()
+		{
+			SimAVR.engineInit("atmega32u4");
+			while(GtkRunning && SimAVR.fetchN(16000000) > 0)
+			{
+			}
+		}
+
+		public static void Main(string[] args)
 		{
 			Application.Init();
 			MainWindow win = new MainWindow();
 			win.ShowAll();
-			SimAVR.loadPartialProgram(null);
+			string[] lines = System.IO.File.ReadAllLines(args[0]);
+			foreach(string line in lines)
+			{
+				var encoding = Encoding.UTF8;
+				byte[] bytes = new byte[line.Length];
+				System.Buffer.BlockCopy(encoding.GetBytes(line), 0, bytes, 0, bytes.Length);
+				SimAVR.loadPartialProgram(bytes);
+			}
+			new Thread(Simulation).Start();
 			Application.Run();
+			GtkRunning = false;
 		}
 	}
 }
